68e8f7ff2615d9fb1c86f163aa6ff8b7f02b4833
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-06-30 20:11:35 -0400

    wrote blank tests for Board.init and .build_board

---
+++ spec/chess_spec.rb
@@ -0,0 +1,37 @@
+require './lib/chess.rb'
+
+describe Board do
+  subject(:board) { Board.new }
+
+  describe "#initialize" do
+
+    it "creates a board obj" do
+
+    end
+
+    it "assigns a value to @board" do
+
+    end
+
+  end
+
+  describe "#build_board" do
+    let(:test_board) { subject.build_board }
+
+    it "returns a hash" do
+
+    end
+
+    it "has 64 key/val pairs" do
+
+    end
+
+    it "starts with nil values" do
+
+    end
+
+    it "has keys named according to algebraic chess notation" do
+
+    end
+  end
+end
