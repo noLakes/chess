0fa46635722bae5f90311cba7d7610941eaaf58a
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-06-30 20:58:22 -0400

    created game class with init/build_board

---
+++ lib/chess.rb
@@ -0,0 +1,25 @@
+
+class Game
+  attr_reader :board, :turn, :players
+
+  def initialize
+    @board = build_board
+    @players = {'White' => 'W', 'Black' => 'B'}
+    @turn = @players['White']
+  end
+
+
+  def build_board
+    board = {}
+    files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    ranks = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    files.each do |file|
+      ranks.each do |rank|
+        board["#{file}#{rank}"] = nil
+      end
+    end
+    board
+  end
+
+end
