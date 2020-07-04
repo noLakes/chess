c93c457ce7cc5366102aea7482025be5dc97d4cd
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-07-04 18:21:04 -0400

    added initialize to Bishop class

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -189,3 +189,40 @@ describe Rook do
 
   end
 end
+
+  describe Bishop do
+    subject(:bishop) { Bishop.new }
+    let(:board) { Board.new }
+  
+    describe "#initialize" do
+  
+      it "creates a bishop obj" do
+        expect(subject).to be_kind_of(Bishop)
+      end
+  
+      it "can be assgined a color" do
+        white = Bishop.new('W')
+        black = Bishop.new('B')
+        expect(white.color).to eql("W")
+        expect(black.color).to eql("B")
+      end
+  
+      it "has an instance varable for text representation" do
+        white = Bishop.new('W')
+        black = Bishop.new('B')
+        expect(white.txt).to eql("\u2657")
+        expect(black.txt).to eql("\u265D")
+      end
+  
+      it "has an instance variable for position (default is nil)" do
+        expect(subject.pos).to be_nil
+      end
+  
+      it "has an instance variable for movement increments (array length > 0)" do
+        expect(subject.increments).to be_kind_of(Array)
+        expect(subject.increments.length).to eql(28)
+      end
+  
+    end
+    
+end
