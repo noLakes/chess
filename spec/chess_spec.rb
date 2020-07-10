1393144504614cd0caa23547f34b4f75df046869
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-07-09 20:33:57 -0400

    removed tests for move and check path (re-planning)

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -72,38 +72,6 @@ describe Game do
     end
   end
 
-  describe "#move" do
-    let(:test_game) { subject.new }
-    let(:board) { test_game.board }
-    
-    it "works with a legal move: Rook c1 to c8" do
-      board['c', 1].piece = Rook.new('W', ['c', 1])
-      test_game.move(['c', 1], ['c', 8])
-      expect(board['c', 1].piece).to be_nil
-      expect(board['c', 8].piece).to be_kind_of(Rook)
-    end
-
-    it "works with an illegal move: Rook d1 to c5" do
-      board['d', 1].piece = Rook.new('W', ['d', 1])
-      expect(test_game.move(['d', 1], ['c', 5])).to be_nil
-      expect(board['d', 1].piece).to be_kind_of(Rook)
-    end
-  end
-
-  describe "#check_path" do
-    let(:test_game) { subject.new }
-    let(:board) { test_game.board }
-
-    it "returns true for valid path: Rook c1 to c8" do
-      expect(test_game.check_path(['c', 1], ['c', 8])).to be_truthy
-    end
-
-    it "returns false for invalid path: Rook d1 to c5" do
-      expect(test_game.check_path(['d', 1], ['c', 5])).to be_falsey
-    end
-    
-  end
-
 end
 
 describe Board do
