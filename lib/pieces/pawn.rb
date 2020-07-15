0f26a234198bd5fad66b59d19608ca51ca0c00a5
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-07-14 20:57:26 -0400

    added @moved to pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,12 +3,13 @@ require '/Users/Shan/web-projects/odin_on_rails/ruby_projects/chess/lib/chess_me
 class Pawn
   include Chess_methods
 
-  attr_reader :color, :pos, :txt, :increments, :range, :in_range
+  attr_reader :color, :pos, :txt, :moved :increments, :range, :in_range
 
   def initialize(color = 'W', pos = nil)
     @color = color
     @pos = pos
     @txt = @color == 'W' ? "\u2659" : "\u265F"
+    @moved = false
     @increments = [[0, 1], [0, 2], [-1, 1], [1, 1]].freeze
     @range = [[0, 1], [0, 2], [-1, 1], [1, 1]].freeze
     @in_range = @pos.nil? ? [] : get_in_range(@pos, @range)
@@ -18,6 +19,10 @@ class Pawn
     return nil unless in_board(new_pos)
     @pos = new_pos
     @in_range = get_in_range(@pos, @range)
+  end
+
+  def moved_true
+    @moved = true
   end
 
 end
