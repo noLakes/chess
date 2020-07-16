2f91458fd289a4a2fa9f7b994b3782a0eb7f276f
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-07-16 19:54:54 -0400

    added last_move to pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,13 +3,14 @@ require '/Users/Shan/web-projects/odin_on_rails/ruby_projects/chess/lib/chess_me
 class Pawn
   include Chess_methods
 
-  attr_reader :color, :pos, :txt, :moved, :increments, :range, :in_range
+  attr_reader :color, :pos, :txt, :moved, :last_move, :increments, :range, :in_range
 
   def initialize(color = 'W', pos = nil)
     @color = color
     @pos = pos
     @txt = @color == 'W' ? "\u2659" : "\u265F"
     @moved = false
+    @last_move = nil
     @increments = [[0, 1], [0, 2], [-1, 1], [1, 1]].freeze
     @range = [[0, 1], [0, 2], [-1, 1], [1, 1]].freeze
     @in_range = @pos.nil? ? [] : get_in_range(@pos, @range)
@@ -23,6 +24,10 @@ class Pawn
 
   def moved_true
     @moved = true
+  end
+
+  def last_move=(inc)
+    @last_move = inc
   end
 
 end
